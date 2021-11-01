b5bcc227a4c590c846fcb9357470e0e70b9fcb72
Pasgen-Manley <pasgen95@gmail.com>
2021-11-01 14:49:47 +0800

    completed footer component of portfolio, also added bootstrap dependicies

---
+++ src/components/footer.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { AiFillGithub, AiOutlineTwitter } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>Designed and Developed by Pasgen Manley</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year} Pasgen Manley</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/Pasgen-Manley"
+                style={{ color: "black" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://twitter.com/ManleyPasgen"
+                style={{ color: "black" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiOutlineTwitter />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="www.linkedin.com/in/pasgenmanley"
+                style={{ color: "black" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
