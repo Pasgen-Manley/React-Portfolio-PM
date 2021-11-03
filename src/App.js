8936f705a107375ced63735c3db4dc25f55a188b
Pasgen-Manley <pasgen95@gmail.com>
2021-11-03 14:04:44 +0800

    build out an App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import React from 'react';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { useState} from 'react';
 
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Portfolio from './pages/Portfolio';
-import SingleThought from './pages/SingleThought';
-import Profile from './pages/Profile';
+import Resume from './pages/Resume';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Container from './components/Container';
 
-// Construct our main GraphQL API endpoint
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
+function App () {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    return <Resume />
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return <div>
+  <Header handlePageChange={handlePageChange} currentPage={currentPage}/>
+  <Container renderPage={renderPage} />
+  <Footer />
+
+  </div>
+}
+
+export default App;
