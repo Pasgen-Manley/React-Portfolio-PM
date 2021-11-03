143c963ffe6665c2a6d03bb99dc157f836a34e15
Pasgen-Manley <pasgen95@gmail.com>
2021-11-03 16:51:28 +0800

    imported images in src file and imported into portfolio.js

--- my-portfolio/src/pages/Portfolio.js
+++ my-portfolio/src/pages/Portfolio.js
@@ -1,12 +1,18 @@
 import React from "react";
 import ProjectCards from "../components/ProjectCards";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import TheGoods from "../assets/images/TheGoods.png";
+import SongStar from "../assets/images/SongStar.png";
+import AnytimeBudget from "../assets/images/budget1.png";
+import Fitness from "../assets/images/fitness-tracker.png";
+import TechBlog from "../assets/images/tech-blog.png";
+import NoteTaker from "../assets/images/note-taker.png";
 
 const projects = [
   {
     id: 1,
     name: "TheGoods",
-    image: "../../assets/images/TheGoods.png",
+    image: TheGoods,
     github: "https://github.com/Pasgen-Manley/TheGoods",
     deploy: "https://morning-ravine-85802.herokuapp.com/",
     topics: "Node.JS, Sequelize, Handlebars, Express.js, MySQL, MVC, dotenv, Gulp"
@@ -14,7 +20,7 @@ const projects = [
   {
     id: 2,
     name: "SongStar",
-    image: "../../public/assets/images/SongStar.png",
+    image: SongStar,
     github: "https://github.com/scottyv6/SongStar",
     deploy: "https://scottyv6.github.io/SongStar/",
     topics: "JS, Bulma, jQuery"
@@ -22,7 +28,7 @@ const projects = [
   {
     id: 3,
     name: "AnytimeBudget",
-    image: "../../public/assets/images/budget1.png",
+    image: AnytimeBudget,
     github: "https://github.com/Pasgen-Manley/Anytime-Budget",
     deploy: "https://floating-cove-02210.herokuapp.com/",
     topics: "JS, Mongoose, PWA, Express, Node.js"
@@ -30,7 +36,7 @@ const projects = [
   {
     id: 4,
     name: "Fitness Tracker",
-    image: "../../public/assets/images/fitness-tracker.png",
+    image: Fitness,
     github: "https://github.com/Pasgen-Manley/Workout-Tracker",
     deploy: " https://stormy-plateau-47635.herokuapp.com/",
     topics: "JS, Mongoose, PWA, Express, Node.js"
@@ -38,7 +44,7 @@ const projects = [
   {
     id: 5,
     name: "Tech Blog",
-    image: "../../public/assets/images/tech-blog.png",
+    image: TechBlog,
     github: "https://github.com/Pasgen-Manley/Tech-Blog",
     deploy: "https://pure-plains-74715.herokuapp.com/login",
     topics: "JS, Sequalize, HandleBars, Express, MySQL, Dotenv, Node.js"
@@ -46,7 +52,7 @@ const projects = [
   {
     id: 6,
     name: "Note Taker",
-    image: "../../assets/images/note-taker.png",
+    image: NoteTaker,
     github: "https://github.com/Pasgen-Manley/Note-Taker-Write-and-Save-Notes",
     deploy: "https://shrouded-shore-32119.herokuapp.com/",
     topics: "JS, Sequalize, Express, MySQL, Dotenv, Node.js"
